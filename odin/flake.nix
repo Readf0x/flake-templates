b3e302352c12cfebdc68073fcdfd80c0351627de
readf0x <davis.a.forsythe@gmail.com>
2026-01-06 13:06:31 -0600

    update odin flake

--- odin/flake.nix
+++ odin/flake.nix
@@ -7,46 +7,55 @@ rec {
   outputs = inputs @ {flake-parts, ...}:
     flake-parts.lib.mkFlake {inherit inputs;} {
       systems = ["x86_64-linux"];
-      perSystem = {
-        system,
-        pkgs,
-        ...
-      }: let
-        info = {
-          projectName = null;
+      perSystem = { system, pkgs, lib, ... }: let info = {
+        projectName = null;
+        dependencies = with pkgs; [];
+      }; in ({
+        projectName,
+        dependencies ? [],
+        sourceDir ? ".",
+        binaryName ? projectName
+      }: rec {
+        devShells.default = pkgs.mkShell {
+          packages = with pkgs; [
+            odin
+            gnumake
+            packages.init-proj
+          ] // dependencies;
         };
-      in
-        ({
-          projectName,
-        }: rec {
-          devShells.default = pkgs.mkShell {
-            packages = with pkgs; [
-              odin
-              packages.init-proj
-            ];
-          };
-          packages = {
-            ${projectName} = pkgs.stdenv.mkDerivation rec {
-              name = projectName;
-              pname = name;
-              version = "0.1";
-              src = ./.;
-              nativeBuildInputs = [];
-              buildInputs = [];
-              meta = {
-                inherit description;
-                # homepage = "";
-                # license = lib.licenses.;
-                # maintainers = with lib.maintainers; [  ];
-              };
+        packages = {
+          ${projectName} = pkgs.stdenv.mkDerivation rec {
+            pname = projectName;
+            version = "0.1";
+            src = ./.;
+            nativeBuildInputs = [];
+            buildInputs = [];
+            meta = {
+              inherit description;
+              homepage = "";
+              license = lib.licenses.gpl3Only;
             };
-            default = packages.${projectName};
-            init-proj = pkgs.writeShellScriptBin "init-proj" ''
-              git init
-              git add -A
-            '';
           };
-        })
-        info;
+          default = packages.${projectName};
+          init-proj = pkgs.writeShellScriptBin "init-proj" ''
+            git init
+            git add -A
+            cat > makefile <<EOF
+            PREFIX ?= /usr/local
+            BINDIR ?= $(PREFIX)/bin
+            MANDIR ?= $(PREFIX)/share/man
+
+            build:
+              odin build ${sourceDir} -out:${binaryName}
+            
+            debug:
+              odin build ${sourceDir} -debug -out:${binaryName}
+
+            install: build
+              install -Dm755 ${binaryName} "$(DESTDIR)$(BINDIR)/${binaryName}"
+            EOF
+          '';
+        };
+      }) info;
     };
 }
