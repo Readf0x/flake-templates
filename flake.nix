56d63b04f7f574d2bd83641ce70784b90ec57000
readf0x <readf0x@duck.com>
2026-03-05 14:44:34 -0600

    refactored + removed node

--- flake.nix
+++ flake.nix
@@ -1,36 +1,16 @@
 {
   description = "A collection of flake templates";
-
-  outputs = { self }: {
-    templates = {
-
-      generic = {
-        path = ./generic;
-        description = "Set up for adding your own stuff";
+  outputs = {self}: {
+    templates =
+      builtins.mapAttrs (name: value: {
+        path = ./${name};
+        description = value;
+      }) {
+        generic = "Set up for adding your own stuff";
+        go = "Golang template";
+        go-qt = "miQT template";
+        ocaml = "OCaml template (nix managed)";
       };
-
-      go = {
-        path = ./go;
-        description = "Golang template";
-      };
-
-      go-qt = {
-        path = ./go-qt;
-        description = "miQT template";
-      };
-
-      node = {
-        path = ./node;
-        description = "Node.js template";
-      };
-
-      ocaml = {
-        path = ./ocaml;
-        description = "OCaml template (nix managed)";
-      };
-
-    };
-
     defaultTemplate = self.templates.generic;
   };
 }
--- go-qt/flake.nix
+++ go-qt/flake.nix
@@ -1,4 +1,4 @@
-{
+rec {
   description = "Description for the project";
 
   inputs = {
@@ -6,19 +6,58 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
-  outputs = inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" ];
-      perSystem = { system, pkgs, ... }: {
-        devShells.default = pkgs.mkShell {
-          GOPATH = "/home/readf0x/.config/go";
-          packages = with pkgs; [
-            go
-            delve
-            kdePackages.full
-            pkg-config
-          ];
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      perSystem = {
+        system,
+        pkgs,
+        ...
+      }: let
+        info = {
+          projectName = null; # Don't forget to change this!
+          # You can set the module name as well
+          # moduleName = "github.com/example/${projectName}";
         };
-      };
+      in
+        (
+          {
+            projectName,
+            moduleName ? projectName,
+          }: rec {
+            devShells.default = pkgs.mkShell {
+              packages = with pkgs; [
+                go
+                delve
+                kdePackages.full
+                pkg-config
+              ];
+            };
+            packages = {
+              ${projectName} = pkgs.buildGoModule rec {
+                name = projectName;
+                pname = name;
+                version = "0.1";
+
+                src = ./.;
+
+                vendorHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+
+                meta = {
+                  inherit description;
+                  # homepage = "";
+                  # license = lib.licenses.;
+                  # maintainers = with lib.maintainers; [  ];
+                };
+              };
+              default = packages.${projectName};
+              init-script = pkgs.writeShellScriptBin "init-proj" ''
+                git init
+                go mod init ${moduleName}
+              '';
+            };
+          }
+        )
+        info;
     };
 }
